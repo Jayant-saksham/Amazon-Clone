4f6d983204aaae02a27d4e61331b51e5b9b54e6d
Jayant <Jayant2410@gmail.com>
2020-10-04 21:15:05 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDwBsXLXBwaDI1YjNPwVWv3d5O1v8c3h14",
+    authDomain: "fir-cba97.firebaseapp.com",
+    databaseURL: "https://fir-cba97.firebaseio.com",
+    projectId: "fir-cba97",
+    storageBucket: "fir-cba97.appspot.com",
+    messagingSenderId: "395771974955",
+    appId: "1:395771974955:web:c665f7d145f7f3374ba16e",
+    measurementId: "G-3FWDPZR8MG"
+  };
